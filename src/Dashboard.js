081c83ae8fb2c35447a0ab879e1951338c97d917
Eushen Jungaya <Eushen.Jungaya@gbs.local>
2023-09-26 17:08:26 +0800

    fix issue on missing cart

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -299,11 +299,12 @@ const Dashboard = () => {
                 <div>
                     <div id="landingId">
                         {/*landing page */}
-                        <div id="landingwrapper">
+                        <div className="landingwrapper">
                             <div className="landing-navbar">
                                 <div className="display-flex">
                                     <h1 className="landing-header">Paragon
-                                        <span className="purple">Hub</span></h1>
+                                        <span className="purple">Hub</span>
+                                    </h1>
                                 </div>
                                 <div>
                                     <button type="button">Home</button>
@@ -316,9 +317,10 @@ const Dashboard = () => {
                                     <button className="svg-button">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                             <path id="Vector" d="M3 3H3.26835C3.74213 3 3.97943 3 4.17267 3.08548C4.34304 3.16084 4.48871 3.28218 4.59375 3.43604C4.71269 3.61026 4.75564 3.8429 4.84137 4.30727L7.00004 16L17.4218 16C17.875 16 18.1023 16 18.29 15.9199C18.4559 15.8492 18.5989 15.7346 18.7051 15.5889C18.8252 15.4242 18.8761 15.2037 18.9777 14.7631L18.9785 14.76L20.5477 7.95996L20.5481 7.95854C20.7023 7.29016 20.7796 6.95515 20.6947 6.69238C20.6202 6.46182 20.4635 6.26634 20.2556 6.14192C20.0184 6 19.6758 6 18.9887 6H5.5M18 21C17.4477 21 17 20.5523 17 20C17 19.4477 17.4477 19 18 19C18.5523 19 19 19.4477 19 20C19 20.5523 18.5523 21 18 21ZM8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20C9 20.5523 8.55228 21 8 21Z" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                        </svg></button>
+                                        </svg>
+                                    </button>
                                 </div>
-                                </div>
+                            </div>
                         </div>
                         {/*landing page body*/}
                         <div className="outer-container">
@@ -379,7 +381,90 @@ const Dashboard = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-4 cart-container">
+                    {/* <div className="col-md-4 cart-container">
+                        {
+                            cartList.length > 0 ? (
+                                <div>
+                                    <div className="card cart-wrapper box-shadow">
+                                        <h4>Your Cart</h4>
+                                        <div className="shopcart-icon">
+                                            <FiShoppingCart size={28} color="#ffff" />
+                                        </div>
+                                        <div>
+                                            {
+                                                cartList.map((item, key) => {
+                                                    if (!itemTitles.includes(item.title)) {
+                                                        itemTitles.push(item.title)
+
+                                                        keyCounts[item.title] = {
+                                                            title: item.title,
+                                                            quantity: 1,
+                                                            price: item.price * 1,
+                                                            image: item.image,
+
+
+                                                        };
+                                                    } else {
+                                                        keyCounts[item.title] = {
+                                                            title: item.title,
+                                                            quantity: parseInt(keyCounts[item.title].quantity) + 1,
+                                                            price: parseInt(item.price) * (parseInt(keyCounts[item.title].quantity) + 1),
+                                                            image: item.image,
+
+                                                        };
+                                                    }
+                                                })
+                                            }
+
+                                            {
+                                                Object.values(keyCounts).map((item, key) => {
+                                                    return <div className="row">
+
+                                                        <div className="col-3"><img src={item.image} />
+                                                        </div>
+                                                        <div className="col-5">
+                                                            {item.title} {item.quantity > 1 ? "x" + item.quantity : ""}
+                                                        </div>
+                                                        <div className="col-3">
+                                                            {formatPHPCurrency(item.price)}
+                                                        </div>
+                                                        <div className="col-1 delete-icon">
+                                                            <FaRegTrashAlt color="#8a2be2" onClick={() => removeToCart(key, item.title)} size={24} />
+                                                        </div>
+                                                    </div>
+
+                                                })
+                                            }
+
+                                            <div className="shopcart-border" />
+                                            <div className="row">
+
+                                                <div className="col-5">
+                                                    <b>Total:</b>
+                                                </div>
+                                                <div className="col-7">
+                                                    <b>{formatPHPCurrency(total)}</b>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                        <div className="container">
+                                            <div>
+                                                <div className="form-group">
+                                                    <input onChange={(event) => inputChangedHandler(event.target.value, "email")} type="email" className="form-control" id="email" placeholder="Enter your email" />
+                                                </div>
+                                                <button onClick={() => checkOut()} type="button">Checkout</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ) : null
+                        }
+                    </div> */}
+                </div>
+            </div>
+            
+            <div className="cart-container">
                         {
                             cartList.length > 0 ? (
                                 <div>
@@ -459,8 +544,6 @@ const Dashboard = () => {
                             ) : null
                         }
                     </div>
-                </div>
-            </div>
         </div>
     );
 };
