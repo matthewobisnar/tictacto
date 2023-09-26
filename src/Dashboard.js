172f049424c24549017492fe742db7069f0556c9
Eushen Jungaya <Eushen.Jungaya@gbs.local>
2023-09-26 16:38:23 +0800

    ui fixes on landing page

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 import "./App.scss";
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import skullcandyImage from './images/skullcandy.png';
 import nanoleafImage from './images/nanoleaf.jpg';
 import macbookairImage from './images/macbookair.jpg';
@@ -15,10 +15,9 @@ import keyboardImage from './images/keyboard.jpg';
 import lightpanelsImage from './images/lightpanel.jpg';
 import landingpageImage from './images/landingbg.png';
 import landingeagleImage from './images/landingeagle.png';
-import { useHistory } from "react-router-dom";
 
 import { Button } from 'react-bootstrap';
-import { FaRegTrashAlt} from "react-icons/fa";
+import { FaRegTrashAlt } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
 import { Link } from "@mui/material";
 
@@ -28,8 +27,6 @@ const socket = io.connect("http://localhost:3002");
 const Dashboard = () => {
 
     let keyCounts = {};
-    
-    const history = useHistory();
 
     let productList = [
         {
@@ -137,7 +134,7 @@ const Dashboard = () => {
         let data = {
             "title": title,
             "price": price,
-            "image":image,
+            "image": image,
             "room": roomKey
         }
 
@@ -201,15 +198,11 @@ const Dashboard = () => {
             });
         }
     }
-    
+
     const inputChangedHandler = (value, input) => {
         if (input === "email") {
             setEmail(value);
         }
-    };
-
-    const handleButtonClick = () => {
-        history.push("/order");
     };
 
     const checkOut = async () => {
@@ -237,14 +230,14 @@ const Dashboard = () => {
 
     function formatPHPCurrency(amount, locale = 'en-PH') {
         const options = {
-          style: 'currency',
-          currency: 'PHP', // ISO currency code for Philippine Peso
-          currencyDisplay: 'symbol', // Display currency symbol (₱)
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2,
+            style: 'currency',
+            currency: 'PHP', // ISO currency code for Philippine Peso
+            currencyDisplay: 'symbol', // Display currency symbol (₱)
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
         };
 
-    
+
         return new Intl.NumberFormat(locale, options).format(amount);
     }
 
@@ -279,61 +272,63 @@ const Dashboard = () => {
     //     }
     // })
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const image = document.createElement('img');
-        image.src = landingeagleImage;
-        image.alt = 'eagle'
-        image.style.marginTop = '55px';
-        const rightCol = document.querySelector('.right-col');
-        rightCol.appendChild(image);
-      });
-  
-      document.addEventListener('click', function(event) {
-        const target = event.target;
-        if (target.classList.contains('shopnow')) {
-          const dFlexElement = target.closest('.d-flex');
-          if (dFlexElement) {
-            dFlexElement.scrollIntoView({ behavior: 'smooth' });
-          }
-        }
-      }); 
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     const image = document.createElement('img');
+    //     image.src = landingeagleImage;
+    //     image.alt = 'eagle'
+    //     image.style.marginTop = '55px';
+    //     const rightCol = document.querySelector('.right-col');
+    //     rightCol.appendChild(image);
+    // });
+
+    // document.addEventListener('click', function (event) {
+    //     const target = event.target;
+    //     if (target.classList.contains('shopnow')) {
+    //         const dFlexElement = target.closest('.d-flex');
+    //         if (dFlexElement) {
+    //             dFlexElement.scrollIntoView({ behavior: 'smooth' });
+    //         }
+    //     }
+    // });
 
 
-    return (     
+    return (
         <div className="shop-body">
 
             <div className="landingpage">
                 <div>
                     <div id="landingId">
-                            {/*landing page */}
-                            <div id="landingwrapper">
-                            <div class="landing-navbar">
-                            <div class="display-flex">
-                            <h1 class="landing-header">Paragon
-                            <span class="purple">Hub</span></h1>
-                            </div>
-                            <div>
-                            <button type="button">Home</button>
-                            <button type="button">Products</button>
-                            <button type="button">About</button>
-                            <button class="svg-button">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 1024 1024">
-                            <path d="M448 768A320 320 0 1 0 448 128a320 320 0 0 0 0 640z m297.344-76.992l214.592 214.592-54.336 54.336-214.592-214.592a384 384 0 1 1 54.336-54.336z" fill="#FFFFFF"/>
-                            </svg></button>
-                            <button class="svg-button">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-                            <path id="Vector" d="M3 3H3.26835C3.74213 3 3.97943 3 4.17267 3.08548C4.34304 3.16084 4.48871 3.28218 4.59375 3.43604C4.71269 3.61026 4.75564 3.8429 4.84137 4.30727L7.00004 16L17.4218 16C17.875 16 18.1023 16 18.29 15.9199C18.4559 15.8492 18.5989 15.7346 18.7051 15.5889C18.8252 15.4242 18.8761 15.2037 18.9777 14.7631L18.9785 14.76L20.5477 7.95996L20.5481 7.95854C20.7023 7.29016 20.7796 6.95515 20.6947 6.69238C20.6202 6.46182 20.4635 6.26634 20.2556 6.14192C20.0184 6 19.6758 6 18.9887 6H5.5M18 21C17.4477 21 17 20.5523 17 20C17 19.4477 17.4477 19 18 19C18.5523 19 19 19.4477 19 20C19 20.5523 18.5523 21 18 21ZM8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20C9 20.5523 8.55228 21 8 21Z" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg></button>
-                            </div></div>
+                        {/*landing page */}
+                        <div id="landingwrapper">
+                            <div className="landing-navbar">
+                                <div className="display-flex">
+                                    <h1 className="landing-header">Paragon
+                                        <span className="purple">Hub</span></h1>
+                                </div>
+                                <div>
+                                    <button type="button">Home</button>
+                                    <button type="button">Products</button>
+                                    <button type="button">About</button>
+                                    <button className="svg-button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 1024 1024">
+                                            <path d="M448 768A320 320 0 1 0 448 128a320 320 0 0 0 0 640z m297.344-76.992l214.592 214.592-54.336 54.336-214.592-214.592a384 384 0 1 1 54.336-54.336z" fill="#FFFFFF" />
+                                        </svg></button>
+                                    <button className="svg-button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                                            <path id="Vector" d="M3 3H3.26835C3.74213 3 3.97943 3 4.17267 3.08548C4.34304 3.16084 4.48871 3.28218 4.59375 3.43604C4.71269 3.61026 4.75564 3.8429 4.84137 4.30727L7.00004 16L17.4218 16C17.875 16 18.1023 16 18.29 15.9199C18.4559 15.8492 18.5989 15.7346 18.7051 15.5889C18.8252 15.4242 18.8761 15.2037 18.9777 14.7631L18.9785 14.76L20.5477 7.95996L20.5481 7.95854C20.7023 7.29016 20.7796 6.95515 20.6947 6.69238C20.6202 6.46182 20.4635 6.26634 20.2556 6.14192C20.0184 6 19.6758 6 18.9887 6H5.5M18 21C17.4477 21 17 20.5523 17 20C17 19.4477 17.4477 19 18 19C18.5523 19 19 19.4477 19 20C19 20.5523 18.5523 21 18 21ZM8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20C9 20.5523 8.55228 21 8 21Z" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                        </svg></button>
+                                </div>
+                                </div>
                         </div>
-                            {/*landing page body*/}
-                        <div class="outer-container">
-                            <div class="left-col">
-                                <h5 class="landing-bodyheader">Empowering the world with latest <span class="landing-bodyheader purple">Top Of The Line</span><span class="landing-bodyheader"> Tech</span> </h5>
-                                <p class="landing-bodyheader">Selling High-Quality, State-Of-The-Art Technologies and Equipment Guaranteed To Make Living Your Everyday Life <span class="orange"> 120% </span> Better</p>
+                        {/*landing page body*/}
+                        <div className="outer-container">
+                            <div className="left-col">
+                                <h5 className="landing-bodyheader">Empowering the world with latest <span className="landing-bodyheader purple">Top Of The Line</span><span className="landing-bodyheader"> Tech</span> </h5>
+                                <p className="landing-bodyheader">Selling High-Quality, State-Of-The-Art Technologies and Equipment Guaranteed To Make Living Your Everyday Life <span className="orange"> 120% </span> Better</p>
                                 <button className="shopnow">Shop Now</button>
                             </div>
-                            <div class="right-col">
+                            <div className="right-col">
+                                <img className="right-img" src={landingeagleImage} alt="Eagle"/>
                             </div>
                         </div>
                     </div>
@@ -357,22 +352,22 @@ const Dashboard = () => {
                                                 <img className="card-img-top" src={item.image} alt="NBA 2k23" />
                                                 <div className="card-body">
                                                     <h5 className="card-title">{item.title}</h5>
-                                                    <div class="inner-card-text">
+                                                    <div className="inner-card-text">
                                                         <p className="card-text">{item.description}</p>
-                                                            <div class="rate">
-                                                                <input type="radio" id="star5" name="rate" value="5" />
-                                                                <label for="star5" title="text">5 stars</label>
-                                                                <input type="radio" id="star4" name="rate" value="4" />
-                                                                <label for="star4" title="text">4 stars</label>
-                                                                <input type="radio" id="star3" name="rate" value="3" />
-                                                                <label for="star3" title="text">3 stars</label>
-                                                                <input type="radio" id="star2" name="rate" value="2" />
-                                                                <label for="star2" title="text">2 stars</label>
-                                                                <input type="radio" id="star1" name="rate" value="1" />
-                                                                <label for="star1" title="text">1 star</label>
-                                                            </div>
-                                                        <large className="text-price">{formatPHPCurrency(item.price)}</large>
-                                                        <div className="add-to-cart">                                                        
+                                                        <div className="rate">
+                                                            <input type="radio" id="star5" name="rate" value="5" />
+                                                            <label for="star5" title="text">5 stars</label>
+                                                            <input type="radio" id="star4" name="rate" value="4" />
+                                                            <label for="star4" title="text">4 stars</label>
+                                                            <input type="radio" id="star3" name="rate" value="3" />
+                                                            <label for="star3" title="text">3 stars</label>
+                                                            <input type="radio" id="star2" name="rate" value="2" />
+                                                            <label for="star2" title="text">2 stars</label>
+                                                            <input type="radio" id="star1" name="rate" value="1" />
+                                                            <label for="star1" title="text">1 star</label>
+                                                        </div>
+                                                        <b className="text-price">{formatPHPCurrency(item.price)}</b>
+                                                        <div className="add-to-cart">
                                                             <Button onClick={() => addToCart(item.title, item.price, item.image)} variant="primary">Add to cart</Button>
                                                         </div>
                                                     </div>
@@ -396,7 +391,6 @@ const Dashboard = () => {
                                         <div>
                                             {
                                                 cartList.map((item, key) => {
-
                                                     if (!itemTitles.includes(item.title)) {
                                                         itemTitles.push(item.title)
 
@@ -404,17 +398,17 @@ const Dashboard = () => {
                                                             title: item.title,
                                                             quantity: 1,
                                                             price: item.price * 1,
-                                                            image:item.image,
-                                                            
-                                                    
+                                                            image: item.image,
+
+
                                                         };
                                                     } else {
                                                         keyCounts[item.title] = {
                                                             title: item.title,
                                                             quantity: parseInt(keyCounts[item.title].quantity) + 1,
                                                             price: parseInt(item.price) * (parseInt(keyCounts[item.title].quantity) + 1),
-                                                            image:item.image,
-                                                            
+                                                            image: item.image,
+
                                                         };
                                                     }
                                                 })
@@ -427,7 +421,7 @@ const Dashboard = () => {
                                                         <div className="col-3"><img src={item.image} />
                                                         </div>
                                                         <div className="col-5">
-                                                             {item.title} {item.quantity > 1 ? "x" + item.quantity : ""}
+                                                            {item.title} {item.quantity > 1 ? "x" + item.quantity : ""}
                                                         </div>
                                                         <div className="col-3">
                                                             {formatPHPCurrency(item.price)}
@@ -436,20 +430,20 @@ const Dashboard = () => {
                                                             <FaRegTrashAlt color="#8a2be2" onClick={() => removeToCart(key, item.title)} size={24} />
                                                         </div>
                                                     </div>
-                                                    
+
                                                 })
                                             }
 
                                             <div className="shopcart-border" />
                                             <div className="row">
-                   
+
                                                 <div className="col-5">
                                                     <b>Total:</b>
                                                 </div>
                                                 <div className="col-7">
                                                     <b>{formatPHPCurrency(total)}</b>
                                                 </div>
-                                        
+
                                             </div>
                                         </div>
                                         <div className="container">
