ae659cb271901ae106cf229f1a9d71ee7805914c
Lady-Diane-Prado <92840695+Lady-Diane-Prado@users.noreply.github.com>
2023-10-03 13:57:56 +0800

    updated product modal design

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -713,7 +713,7 @@ const Dashboard = () => {
                                                                     </Modal.Header>
                                                                     <Modal.Body>
                                                                         <div className="video-player">
-                                                                            <video height="250" controls loop autoPlay muted>
+                                                                            <video width="100%" height="250" controls loop autoPlay muted>
                                                                                 <source src={item.video} type="video/mp4" />
                                                                                 Your browser does not support the video tag.
                                                                             </video>
