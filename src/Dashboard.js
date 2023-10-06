67d2adad3d816ab421ec56053cfcc618987fac1b
ZachDiaz <faptasticcc@gmail.com>
2023-10-06 12:08:29 +0800

    fixed

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -770,7 +770,6 @@ const Dashboard = () => {
                                 <div className="display-flex desktopnavbar">
                                     <button type="button" className={isClassRemoved ? '' : ''}>Home</button>
                                     <button type="button" onClick={handleButtonClick}>Products</button>
-                                    <button type="button">About</button>
                                     <div id="searchContainer" className="display-flex justify" style={{ display: 'none' }}>
                                     <input type="text" id="searchInput" className="searchbar" placeholder="Search..." style={{ display: 'none' }} />
                                     <button id="searchButton" className="searchbutton" style={{ display: 'none' }}>
@@ -815,19 +814,6 @@ const Dashboard = () => {
                                     <div className="menu">
                                     <button type="button" className={isClassRemoved ? '' : ''}>Home</button>
                                     <button type="button" onClick={handleButtonClick}>Products</button>
-                                    <button type="button">About</button>
-                                        <div id="searchContainer" className="display-flex justify" style={{ display: 'none' }}>
-                                    <input type="text" id="searchInput" placeholder="Search..." style={{ display: 'none' }} />
-                                    <button id="searchButton" style={{ display: 'none' }}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 1024 1024">
-                                            <path d="M448 768A320 320 0 1 0 448 128a320 320 0 0 0 0 640z m297.344-76.992l214.592 214.592-54.336 54.336-214.592-214.592a384 384 0 1 1 54.336-54.336z" fill="#FFFFFF" />
-                                        </svg>
-                                    </button>
-                                    </div>
-                                    <button className="svg-button" id="showSearchButton">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 1024 1024">
-                                            <path d="M448 768A320 320 0 1 0 448 128a320 320 0 0 0 0 640z m297.344-76.992l214.592 214.592-54.336 54.336-214.592-214.592a384 384 0 1 1 54.336-54.336z" fill="#FFFFFF" />
-                                        </svg></button>
                                     <button className="svg-button" onClick={() => { toggleValue(); resetTotal(); }}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                             <path id="Vector" d="M3 3H3.26835C3.74213 3 3.97943 3 4.17267 3.08548C4.34304 3.16084 4.48871 3.28218 4.59375 3.43604C4.71269 3.61026 4.75564 3.8429 4.84137 4.30727L7.00004 16L17.4218 16C17.875 16 18.1023 16 18.29 15.9199C18.4559 15.8492 18.5989 15.7346 18.7051 15.5889C18.8252 15.4242 18.8761 15.2037 18.9777 14.7631L18.9785 14.76L20.5477 7.95996L20.5481 7.95854C20.7023 7.29016 20.7796 6.95515 20.6947 6.69238C20.6202 6.46182 20.4635 6.26634 20.2556 6.14192C20.0184 6 19.6758 6 18.9887 6H5.5M18 21C17.4477 21 17 20.5523 17 20C17 19.4477 17.4477 19 18 19C18.5523 19 19 19.4477 19 20C19 20.5523 18.5523 21 18 21ZM8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20C9 20.5523 8.55228 21 8 21Z" stroke="#FFFFFF" fill="transparent" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -1236,25 +1222,8 @@ const Dashboard = () => {
                 ) : null
             }
             </div>
-            
-            <div className="landingpage">
-<div>
-    <div id="landingId" className="aboutus">
-        {/*landing page */}
-        <div className="landingwrapper">
-        <div className="d-flex">
-                                <h1 className="firstword-title">About</h1>
-                                <h1 className="secondword-title orange">Us</h1>
-                            </div>
-                            <div className="display-flex">
-                            <p className="landing-bodyheader aboutus-text"> We're a bunch of tech enthusiasts who love making cool stuff with computers. We come from different backgrounds, but our common bond is our passion for all things tech, and we want to share it with you!</p>
-                            <div className="display-grid">
-                            
-                            </div></div></div></div></div></div>
            
         </div>
-
-
     );
 };
 
